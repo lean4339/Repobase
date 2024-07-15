9b3427b3a7dc88f066cb6c07cf7e1ac6f5095394
Leandro Ayala <layala@toolboxtve.com>
2024-07-15 16:55:00 -0300

    validando formulario

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -33,9 +33,13 @@ export const Contact = () => {
     let isError = false;
     for (const key in formData) {
       if (formData[key] === '' && key !== 'company') {
-        newError[key] = 'Este campo es obligatorio';
-        isError = true;
+          newError[key] = 'Este campo es obligatorio';
+          isError = true;
       }
+    }
+    if(formData.email !== '' && !formData.email.includes('@')){
+      newError.email = 'El email no es válido';
+      isError = true;
     }
     if (isError) {
       setError(newError)
@@ -60,20 +64,20 @@ export const Contact = () => {
           <h1 className='title-form'>Contacto</h1>
           <p className='text-form'>Puedes dejarnos un mensaje, o bien, seguirnos en redes sociales</p>
         </div>
-        <input placeholder='Name' autoComplete="off" name='name' id='name' onChange={handleChange} type='text' label='Nombre' className='inputs'  />
+        <input value={formData.name} placeholder='Name' autoComplete="off" name='name' id='name' onChange={handleChange} type='text' label='Nombre' className='inputs'  />
         {error && error.name &&
           <p className='error'>{error.name}</p>
         }
-        <input placeholder='Email' autoComplete="off" name='email' id='email' onChange={handleChange} type='email' label='Email' className='inputs'  />
+        <input value={formData.email} placeholder='Email' autoComplete="off" name='email' id='email' onChange={handleChange} type='email' label='Email' className='inputs'  />
         {error && error.email &&
           <p className='error'>{error.email}</p>
         }
-        <input placeholder='Telephone' autoComplete="off" name='telephone' id='telephone' onChange={handleChange} type='tel' label='Teléfono' className='inputs'  />
+        <input value={formData.telephone} placeholder='Telephone' autoComplete="off" name='telephone' id='telephone' onChange={handleChange} type='tel' label='Teléfono' className='inputs'  />
         {error && error.telephone &&
           <p className='error'>{error.telephone}</p>
         }
-        <input placeholder='Company' autoComplete="off" name='company' id='company' onChange={handleChange} type='text' label='Empresa (Opcional)' className='inputs'  />
-        <input placeholder='Message' autoComplete="off" name='message' id='message' onChange={handleChange} label='Mensaje o consulta deseada' className='inputs'  rows={6} />
+        <input value={formData.company} placeholder='Company' autoComplete="off" name='company' id='company' onChange={handleChange} type='text' label='Empresa (Opcional)' className='inputs'  />
+        <input value={formData.message} placeholder='Message' autoComplete="off" name='message' id='message' onChange={handleChange} label='Mensaje o consulta deseada' className='inputs'  rows={6} />
         {error && error.message &&
           <p className='error'>{error.message}</p>
         }
