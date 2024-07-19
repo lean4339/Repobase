5c33e728e5fd2a392e352626a2943875500e91e4
Leandro Ayala <layala@toolboxtve.com>
2024-07-19 12:10:47 -0300

    agrgeando logo y cambiando header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,11 +6,11 @@ export const Header = () => {
     const [animatedLinks, setAnimatedLinks] = useState('')
     const [animatedMenu, setAnimatedMenu] = useState(false)
     const links = [
-        {id: 6, title: 'About'},
-        {id: 1, title: 'Services'},
-        {id: 5, title: 'Recruitment'},
+        {id: 6, title: 'Conocenos',link: 'about'},
+        {id: 1, title: 'Servicios', link: 'services'},
+        {id: 5, title: 'Reclutamiento', link: 'recruitment'},
         // {id: 3, title: 'Vacancies'},
-        {id: 4, title: 'Contact'},
+        {id: 4, title: 'Contacto', link: 'contact'},
     ]
     const handleShowMenu = () => {
         setShowMenu(!showMenu)
@@ -23,10 +23,10 @@ export const Header = () => {
 
         <header className={`headerContainer ${animatedMenu? 'animated-menu': 'go-back-menu'}`}>
         <nav className="desktop">
-            <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={'/main'} className="logo hover pointer">Rockstar</LinkRouter>
+            <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={'/main'} className="logo hover pointer">HR Innovate</LinkRouter>
             <ul className="links">
                 {links && links.map(link =>(
-                    <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={`/${link.title.toLowerCase()}`} className="link hover pointer" key={link.id}>{link.title}</LinkRouter>
+                    <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={`/${link.link}`} className="link hover pointer" key={link.id}>{link.title}</LinkRouter>
                 ))}
             </ul>
         </nav>
@@ -34,12 +34,12 @@ export const Header = () => {
         <nav className='mobile'>
             <div className="header-mobile">
 
-            <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={'/main'} className="  pointer hover logo">Rockstar</LinkRouter>
+            <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={'/main'} className="  pointer hover logo">HR Innovate</LinkRouter>
             <div onClick={handleShowMenu} className="menuIcon  pointer hover"><MenuIcon/></div>
             </div>
             {showMenu && <ul className={`links ${animatedLinks}`} >
                 {links && links.map(link =>(
-                     <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={`/${link.title.toLowerCase()}`} className="link hover pointer" key={link.id}>{link.title}</LinkRouter>
+                     <LinkRouter   spy={true} smooth={true} offset={-90} duration={500} to={`/${link.link}`}  className="link hover pointer" key={link.id}>{link.title}</LinkRouter>
                 ))}
             </ul>}
         </nav>
